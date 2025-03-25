61766451e3015aae3b0de29eaeb4b27564c745ae
TheFunkyBanch <vhodlevskyi@gpu-dev.us-central1-a.c.unified-canyon-441519-v8.internal>
2025-03-25 12:33:10 +0000

    delete duplicate

--- src/main.py
+++ src/main.py
@@ -35,7 +35,6 @@ def main():
     
     # df_mcq = df_mcq[common_ids].iloc[:60]
     df_lisa_sheets = df_lisa_sheets[df_lisa_sheets['id'].isin(df_mcq['id'])]
-    df_lisa_sheets = df_lisa_sheets[df_lisa_sheets['id'].isin(df_mcq['id'])]
 
     df_eval = eval_dataframe_parallel(df_mcqs=df_mcq,
                                       df_lisa_sheets=df_lisa_sheets,
@@ -55,7 +54,7 @@ def main():
                                       compute_distractors_quality=True,
                                       distractors_quality_system_prompt=system_prompts['avg_distractors_quality_prompt'],
                                       distractors_quality_col='avg_distractor_quality',
-                                      merge=False # set to True if your dataframe does not have the Lisa Sheet content
+                                      merge=True # set to True if your dataframe does not have the Lisa Sheet content
                                       )
 
     df_eval.to_csv(os.environ.get('MODEL_MCQ_EVAL_EXPORT_PATH'), index=False)
