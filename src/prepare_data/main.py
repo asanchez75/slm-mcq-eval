921102dd26438216bb11cc16eac44a90f63166e6
Nassim Bouarour <32688942+nassim-bou@users.noreply.github.com>
2025-04-11 17:06:24 +0200

    Update main.py

--- src/prepare_data/main.py
+++ src/prepare_data/main.py
@@ -2,7 +2,12 @@ import json
 from mcq_model import create_prompt_chain
 from parallel_generation import run_in_parallel
 from utils import load_test_set, get_all_txt_contents_from_folders
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', "--temperature", type=float, default=0.1)
+
+args = parser.parse_args()
 
 def main():
     path_lisa = "../../data/lisa_sheets"
@@ -12,11 +17,11 @@ def main():
     all_txt_contents = get_all_txt_contents_from_folders(path_lisa)
     
     test_set = load_test_set(all_txt_contents, path_folders=path_test_folders)
-    train_set = load_test_set(all_txt_contents, path_folders=path_train_folders)
+    #train_set = load_test_set(all_txt_contents, path_folders=path_train_folders)
     
-    chain = create_prompt_chain()
+    chain = create_prompt_chain(model_name=model, temperature=args.temperature)
     
-    test_generated_questions = run_in_parallel(test_set[:100], chain, max_workers=10)
+    generated_questions = run_in_parallel(test_set, chain, max_workers=10)
     # train_generated_questions = run_in_parallel(train_set, chain, max_workers=10)
     
     with open("test_generated_questions.json", "w") as f:
